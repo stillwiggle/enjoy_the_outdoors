f6258f8c0fc8bbf05fbe3578837668a8782a8e63
stillwiggle <mike.stillwagon@gmail.com>
2022-06-28 15:32:24 -0400

    Added the ability to search by park type.  Ensured that either option will work provided the drop-down method.  Moved away from radio buttons and to drop-down instead.

--- assets/scripts/parks.js
+++ assets/scripts/parks.js
@@ -1,35 +1,69 @@
 "use strict"
 // This file has locationsArray, nationalParksArray, parkTypesArray available to it because parks.html has load_national_parks_data.js loaded above this file.
 
-// Radio button with 2 options: By Location, By Park Type
+const searchMethod = document.querySelector("#searchMethod");
 const showListButton = document.querySelector("#showList");
-const stateList = document.querySelector("#stateSelection");
-const showByLocation = document.querySelector("#showLocations");
+const listStates = document.querySelector("#stateSelection");
+const showTheParks = document.querySelector("#showParks");
 const showParksList = document.querySelector("#parkList");
+const listTypes = document.querySelector("#typeSelection");
 
+// Drop-down populates if Location is selected as the search method
 showListButton.addEventListener("click", function(event) {
-    locationsArray.forEach(function(state) {
-        stateList.innerHTML += `<option value=\"${state}\">${state}</option>`;
-        document.querySelector("#byLoc").classList.remove("d-none");
-    });
+    
+    showParksList.innerHTML = "";
+    // Search by location
+    if (searchMethod.value === "state") {
+        locationsArray.forEach(function(state) {
+            listStates.innerHTML += `<option value=\"${state}\">${state}</option>`;
+            document.querySelector("#byLoc").classList.remove("d-none");
+            document.querySelector("#byType").classList.add("d-none");
+            showTheParks.classList.remove("d-none");
+        });
+    }
+    // serach by park type
+    else if (searchMethod.value === "type") {
+        parkTypesArray.forEach(function(park) {
+            listTypes.innerHTML += `<option value=\"${park}\">${park}</option>`;
+            document.querySelector("#byType").classList.remove("d-none");
+            document.querySelector("#byLoc").classList.add("d-none");
+            showTheParks.classList.remove("d-none");
+        });
+    }
+    else {
+        alert("You must select an option.")
+    }
+    
 });
 
-showByLocation.addEventListener("click", function(event) {
+// Display parks
+showTheParks.addEventListener("click", function(event) {
     // starts by clearing out all <li> elements within the <ul> to produce a clean list
     showParksList.innerHTML = "";
 
-    // creates a variable to store the state chosen in the drop-down
-    let chosenState = "";
-    chosenState = stateList.value;
+    if (searchMethod.value === "state") {
+        // creates a variable to store the state chosen in the drop-down
+        let chosenState = listStates.value;
     
-    // filters the array of all parks based on the chosen state above, then stores the results in a new array
-    const filteredParksList = nationalParksArray.filter((park) => {
-        return park.State === chosenState;
-    })
+        // filters the array of all parks based on the chosen state above, then stores the results in a new array
+        const filteredParksList = nationalParksArray.filter((park) => {
+            return park.State === chosenState;
+        })
 
-    // using the filtered array above, creates new <li> elements for each park, using only the names of the parks
-    filteredParksList.forEach(function(parks) {
-        showParksList.innerHTML += `<li>${parks.LocationName}</li>`;
-    });
+        // using the filtered array above, creates new <li> elements for each park, using only the names of the parks
+        filteredParksList.forEach(function(parks) {
+            showParksList.innerHTML += `<li>${parks.LocationName}</li>`;
+        });
+    }
+    else if (searchMethod.value === "type") {
+        let chosenType = listTypes.value;
+        const filteredParksList = nationalParksArray.filter((park) => {
+            return park.LocationName.includes(chosenType);
+        })
 
+        // using the filtered array above, creates new <li> elements for each park, using only the names of the parks
+        filteredParksList.forEach(function(parks) {
+            showParksList.innerHTML += `<li>${parks.LocationName}</li>`;
+        });
+    }
 });
