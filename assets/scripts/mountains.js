88764b014f43bf7a4970dedeed1c75fc0c4ba1ac
stillwiggle <mike.stillwagon@gmail.com>
2022-06-29 13:38:59 -0400

    Added sunrise/sunset API call, but it is timing out.  Keeping there for now.

--- assets/scripts/mountains.js
+++ assets/scripts/mountains.js
@@ -7,12 +7,17 @@
 // Global variables for specific DOM elements
 const detailsTable = document.querySelector("#mountainDetails");
 
+
 // When a changes is made to the drop-down selection
 mountainsList.addEventListener("change", (event) => {
     // Clears the data from the table
     detailsTable.innerHTML = "";
     // find the object in the array which matches with the selection in the drop-down
     const chosenMountain = mountainsArray.find(mountain => mountain.name === event.target.value);
+    // sunrise/sunset data - storing as a variable
+    // let sunset = getSunsetForMountain(chosenMountain.coords.lat, chosenMountain.coords.lng).then(sunsetData => {
+    //     console.log(sunsetData.results)
+    // });
     // creates the table row data (2 columns)
     detailsTable.innerHTML += `<tr>
         <td>
@@ -24,8 +29,23 @@ mountainsList.addEventListener("change", (event) => {
                 <li>Description: ${chosenMountain.desc}</li>
                 <li>Elevation: ${chosenMountain.elevation}</li>
                 <li>Effort: ${chosenMountain.effort}</li>
+                <li>Sunrise: </li>
+                <li>Sunset: </li>
             </ul>
         </td>
     </tr>`;
 
-})
+// console.log(chosenMountain.coords.lat);
+});
+
+//function that can "fetch" the sunset/sunrise times
+async function getSunsetForMountain(lat, lng){
+    let response = await fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`)
+    let data = await response.json()
+    return data
+}
+
+// //Using the function to fetch the sunset/sunrise times for a specific mountain 
+// getSunsetForMountain("44.320686", "-71.291742").then(sunsetData => {
+//     console.log(sunsetData.results)
+// });
