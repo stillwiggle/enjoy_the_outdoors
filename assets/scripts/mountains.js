b0cd43befa47b38e5b0491cad183251763d9c2bb
stillwiggle <mike.stillwagon@gmail.com>
2022-06-30 16:59:59 -0400

    Updated comments on both js files.  Added another drop-down for effort.  Created JS to populate the table based on the effort chosen.  Reset drop-down values to default when the other is changed by the user.

--- assets/scripts/mountains.js
+++ assets/scripts/mountains.js
@@ -1,56 +1,87 @@
 "use strict"
-
 // This file has mountainsArray available to it because mountains.html has load_mountains_data.js loaded above this file.
-
-// Mountain efforts: Strenuous, Moderate to Strenuous, Moderate
 
 // Global variables for specific DOM elements
 const detailsTable = document.querySelector("#mountainDetails");
+const effortList = document.querySelector("#effort");
 
-// When a changes is made to the drop-down selection
+// Mountains drop-down: When a changes is made to the selection
 mountainsList.addEventListener("change", (event) => {
+
     // Clears the data from the table
     detailsTable.innerHTML = "";
 
+    // Changes the Effort drop-down to reset to default (avoids confusion)
+    effortList.value = "no-selection";
+
     // creates the table row data (2 columns)
     if(event.target.value !== "no-selection") {
+
         // find the object in the array which matches with the selection in the drop-down
         const chosenMountain = mountainsArray.find(mountain => mountain.name === event.target.value);
 
-        // sunrise/sunset data - storing as a variable
-        // let sunset = getSunsetForMountain(chosenMountain.coords.lat, chosenMountain.coords.lng).then(sunsetData => {
-        //     console.log(sunsetData.results)
-        // });
+        // Runs the function that creates table element HTML
+        populateTable(chosenMountain);
+        
+    }
+});
 
-        detailsTable.innerHTML += `<tr>
+
+// Effort drop-down: when a changes is made to the selection
+effortList.addEventListener("change", (event) => {
+
+    // Clears the data from the table
+    detailsTable.innerHTML = "";
+
+    // Clears Mountains drop-down to reset to default (avoids confusion)
+    mountainsList.value = "no-selection";
+
+    // creates the table row data (2 columns)
+    if(event.target.value !== "no-selection") {
+
+        // find the object in the array which matches with the selection in the drop-down
+        const filteredEffortArray = mountainsArray.filter((mountain) => {
+            return mountain.effort.includes(event.target.value);
+        });
+
+        // Runs the function that creates table element HTML (using forEach since multiple mountains will be selected)
+        filteredEffortArray.forEach((peak) => {
+            populateTable(peak);
+        });
+        
+    }
+});
+
+function populateTable (chosenValue) {
+    return detailsTable.innerHTML += `<tr>
             <td>
-                <h2>${chosenMountain.name}</h2>
-                <img src="/assets/images/mountains/${chosenMountain.img}" alt="View of ${chosenMountain.name}"></img>
+                <h2>${chosenValue.name}</h2>
+                <img src="/assets/images/mountains/${chosenValue.img}" alt="View of ${chosenValue.name}"></img>
             </td>
             <td>
                 <table border="1" bordercolor = "#FFFFFF" class="table-responsive table align-top">
                     <tr>
                         <td>Description:</td>
-                        <td>${chosenMountain.desc}</td>
+                        <td>${chosenValue.desc}</td>
                     </tr>
                     <tr>
                         <td>Elevation:</td>
-                        <td>${chosenMountain.elevation.toLocaleString('en-US')} ft</td>
+                        <td>${chosenValue.elevation.toLocaleString('en-US')} ft</td>
                     </tr>
                     <tr>
                         <td>Effort:</td>
-                        <td>${chosenMountain.effort}</td>
+                        <td>${chosenValue.effort}</td>
                     </tr>
                     <tr>
-                        <td><a href="https://maps.google.com/?q=${chosenMountain.coords.lat},${chosenMountain.coords.lng}" id="mapBtn" title="Google Maps" class="btn btn-primary" target="_blank">Map</a></td>
+                        <td><a href="https://maps.google.com/?q=${chosenValue.coords.lat},${chosenValue.coords.lng}" id="mapBtn" title="Google Maps" class="btn btn-primary" target="_blank">Map</a></td>
                     </tr>
                 </table>
             </td>
         </tr>`;
-    }
-});
+}
 
 //function that can "fetch" the sunset/sunrise times
+// Can't get this working behind LM network
 async function getSunsetForMountain(lat, lng){
     let response = await fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`)
     let data = await response.json()
--- assets/scripts/parks.js
+++ assets/scripts/parks.js
@@ -9,47 +9,61 @@ const showParksList = document.querySelector("#parkList");          // <ul> wher
 
 // Drop-down populates if Location is selected as the search method
 searchMethod.onchange = (event) => {
+
     showParksList.innerHTML = "";
+    
     // Search by location
     if (searchMethod.value === "state") {
+
         locationsArray.forEach(function(state) {
+
             listStates.innerHTML += `<option value=\"${state}\">${state}</option>`;
             document.querySelector("#byLoc").classList.remove("d-none");    // Displays the State (byLoc) drop-down
             document.querySelector("#byType").classList.add("d-none");      // Hides the Park Type (byType) drop-down
-            showTheParks.classList.remove("d-none");                        // Displays the Show Parks button
+            showTheParks.classList.remove("d-none"); 
+                                   // Displays the Show Parks button
         });
     }
     // search by park type
     else if (searchMethod.value === "type") {
+
         parkTypesArray.forEach(function(park) {
+
             listTypes.innerHTML += `<option value=\"${park}\">${park}</option>`;
             document.querySelector("#byType").classList.remove("d-none");   // Displays the Park Type (byType) drop-down
             document.querySelector("#byLoc").classList.add("d-none");       // Hides the State (byLoc) drop-down
             showTheParks.classList.remove("d-none");                        // Displays the Show Parks button
+
         });
     }
     // display all parks (no search)
     else if (event.target.value === "all-Parks") {
+
         showParks(nationalParksArray);
-        document.querySelector("#byType").classList.add("d-none");      // Hides the Park Type (byType) drop-down
-        document.querySelector("#byLoc").classList.add("d-none");       // Hides the State (byLoc) drop-down
-        showTheParks.classList.add("d-none");                           // Hides the Show Parks button
-    }
-    // if blank is selected after another choice has been made
-    else {
         document.querySelector("#byType").classList.add("d-none");          // Hides the Park Type (byType) drop-down
         document.querySelector("#byLoc").classList.add("d-none");           // Hides the State (byLoc) drop-down
         showTheParks.classList.add("d-none");                               // Hides the Show Parks button
+
+    }
+    // if blank is selected after another choice has been made
+    else {
+        
+        document.querySelector("#byType").classList.add("d-none");          // Hides the Park Type (byType) drop-down
+        document.querySelector("#byLoc").classList.add("d-none");           // Hides the State (byLoc) drop-down
+        showTheParks.classList.add("d-none");                               // Hides the Show Parks button
+
     }
 }
 
 // Display parks
 showTheParks.addEventListener("click", function(event) {
+
     // starts by clearing out all <li> elements within the <ul> to produce a clean list
     showParksList.innerHTML = "";
 
     // Filtering method when Location is the search method
     if (searchMethod.value === "state") {
+
         // creates a variable to store the state chosen in the drop-down
         let chosenState = listStates.value;
     
@@ -60,23 +74,30 @@ showTheParks.addEventListener("click", function(event) {
 
         // using the filtered array above, creates new <li> elements for each park, using only the names of the parks
         showParks(filteredParksList);
+
     }
     // Filtering method when Park Type is the search method
     else if (searchMethod.value === "type") {
+
         let chosenType = listTypes.value;
         const filteredParksList = nationalParksArray.filter((park) => {
+
             // uses lower-case to ensure there are matches even if casing is off
             let parkString = park.LocationName.toLowerCase();
             return parkString.includes(chosenType.toLowerCase());
+
         })
 
         // using the filtered array above, creates new <li> elements for each park, using only the names of the parks
         showParks(filteredParksList);
+
     }
 });
 
 function showParks (filteredArray) {
+
     filteredArray.forEach(function(park) {
+        
         // Address field: 0 = no address displayed.  Anything else gets a string
         let address = "";
         if (park.Address !== 0) {
@@ -108,6 +129,7 @@ function showParks (filteredArray) {
             website = `Website: <a href="${park.Visit}" target="_blank">${park.Visit}</a><br>`;
         }
 
+        // Checks to see if al of the contact information does not exist.  If so, prints out a message stating as much.
         if (park.Phone === 0 && park.Fax === 0 && park.hasOwnProperty('Visit') === false) {
             website = `This park does not have any contact information available.<br>`;
         }
